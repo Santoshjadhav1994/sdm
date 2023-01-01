62dc3c5de1477c04d398122317b83b2eb5d37758
Santosh <santoshmjadhav1994@gmail.com>
2023-01-01 21:25:16 +0530

    new app

---
+++ sdm2/server.js
@@ -0,0 +1,14 @@
+var express=require("express");
+var app=express();
+var path=require("path");
+
+//server configuration 
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",function(req,resp){
+    resp.sendFile(path.join(__dirname+"/index.html"));
+});
+
+app.listen(5000);
+console.log("Server is running at the port no 5000");
